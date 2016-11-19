74fa0a7ee6f81232aa6a77f61f52694105c070ef
SunGg12138 <961154209@qq.com>
2016-11-19 23:13:44 +0800

    修改项目

--- project/GoldMiner/js/index.js
+++ project/GoldMiner/js/index.js
@@ -7,9 +7,10 @@ var nowHeight;   //当前绳子的长度
 var stopLoop = true;
 var config = {
 	ropeHeight: 100,   //rope 初始长度
-	maxHeight: mainH-50-40,   //rope 最大长度
+	maxHeight: mainH-30,   //rope 最大长度
 	hookHeight: 20,   //钩子的长度
 	ropeSpeed: 5,   //绳子当前速度
+	nowScore: 0,
 	treasure: {}   //钩到的矿物属性
 };
 $body.keydown(function(e){
@@ -25,6 +26,45 @@ $body.keydown(function(e){
 	};
 });
 // 开始
-var treasures = new Treasures();
+var treasures = new Treasures({
+	types: [
+		{
+			name: '大石头',
+			count: 2,
+			deepRank: 2,
+			score: 50,
+			slowRank: 4,
+			width: 80,
+			style: 'background:#708090;'
+		},
+		{
+			name: '小石头',
+			count: 2,
+			deepRank: 2,
+			score: 20,
+			slowRank: 2,
+			width: 40,
+			style: 'background:#708090;'
+		},
+		{
+			name: '小金块',
+			count: 2,
+			deepRank: 3,
+			score: 200,
+			slowRank: 1,
+			width: 40,
+			style: 'background:rgb(255,215,0);'
+		},
+		{
+			name: '大金块',
+			count: 2,
+			deepRank: 3,
+			score: 500,
+			slowRank: 3,
+			width: 80,
+			style: 'background:rgb(255,215,0);'
+		}
+	]
+});
 ropeRotate();
 treasures.init();
--- project/GoldMiner/js/section/Treasures.js
+++ project/GoldMiner/js/section/Treasures.js
@@ -1,49 +1,62 @@
-var Treasures = function(count) {
+/*
+*treasuresConfig 为 object类型
+*	.types(object) 是宝藏的数量，所在地下的深度，样式
+*		.name 宝藏名字
+*		.count 数量
+*		.deep 深度
+*		.score 分数
+*		.slowRank 减速等级
+*/
+var Treasures = function(treasuresConfig) {
+	this.tags = [];   //所有宝藏的 DOM 元素
 	this.attrs = [];   //所有宝藏的属性
-
-	this.types = [];   //所有宝藏的类型--
-	this.count = count || 10;
-
-	this.tags = [];
+	this.types = treasuresConfig.types;   //所有宝藏的类型
 };
 Treasures.prototype = {
 	init: function() {
-		// this.typesDensity = [];   //宝藏类型的密度
-		// this.typesRarity = [];   //宝藏类型的稀有度
-		for (var i=0;i<this.count;i++) {
-			var thisWidth = getRandom(1,3)*20 + 20;
-			var thisPositionX = getRandom(0, mainW-thisWidth);
-			var thisPositionY = getRandom(config.ropeHeight*2.5, mainH-thisWidth - 50);
-			var tempObj = {originX: thisPositionX+thisWidth/2, originY: thisPositionY+thisWidth/2, width: thisWidth};
-			var thisTag = $('<i class="treasures">');
-			thisTag.css({
-				width: thisWidth,
-				height: thisWidth,
-				background: 'red',
-				left: thisPositionX,
-				top: thisPositionY
-			});
-			thisTag.html(i);
-			$main.append(thisTag);
-			this.tags.push(thisTag);
-			this.attrs.push(tempObj);
-		};
-
+		var thisWidth,DeepPos,thisPositionX,thisPositionY;
+		this.types.forEach(function(item, index){
+			for (var i=0;i<item.count;i++) {
+				thisWidth = item.width;
+				DeepPos = this.getDeepPos(item.deepRank, thisWidth);
+				thisPositionX = getRandom(0, mainW-thisWidth);
+				thisPositionY = getRandom(DeepPos[0], DeepPos[1]);
+				var tempObj = {originX: thisPositionX+thisWidth/2, originY: thisPositionY+thisWidth/2, width: thisWidth};
+				var thisTag = $('<i class="treasures">');
+				thisTag[0].style = item.style;
+				thisTag.css({
+					left: thisPositionX,
+					top: thisPositionY,
+					width: thisWidth,
+					height: thisWidth
+				});
+				thisTag.html(item.name);
+				thisTag.attr('data-slowrank', item.slowRank);
+				thisTag.attr('data-score', item.score);
+				$main.append(thisTag);
+				this.tags.push(thisTag);
+				this.attrs.push(tempObj);
+			};
+		}.bind(this));
 	},
-	clearAll: function(){
+	clearAll: function() {
 		this.tags.forEach(function(item){
 			item.remove();
 		});
 	},
-	clearOne: function(index){
+	clearOne: function(index) {
 		this.tags[index].remove();
 		this.attrs.splice(index, 1);
-		this.types.splice(index, 1);
 		this.tags.splice(index, 1);
-		this.count--;
+	},
+	getDeepPos: function (deepRank, thisWidth) {
+		var minValue = mainH-thisWidth-50-config.ropeHeight*2.5;
+		var maxValue = mainH-thisWidth-50;
+		var preDeep = (maxValue-minValue)/3;
+		return [(deepRank-1)*preDeep+minValue, deepRank*preDeep+minValue];
 	}
 };
-
+//宝藏被拉取时的函数
 function pullTreasure(treasureDOM, treasureAttr) {
 	function tempLoopFunc() {
 		var hookPos = getHookPos();
